aba815af9cc69a0f680e184fd02ca80220aa8c56
unknown <henchants@gmail.com>
2024-04-17 12:15:20 +0800

    data

---
+++ signin.php
@@ -0,0 +1,33 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// connection checking
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Hash password
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+// Insert sa database
+$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
